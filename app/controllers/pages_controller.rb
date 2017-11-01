cb511e9d27c6de19e78b4249e90e4d34897a4c86
moondcr <jodo4x@gmail.com>
2017-11-01 09:41:31 +0800

    整合其他页面

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def stats
+  end
+
+  def pos
+  end
+
+  def wallets
+  end
+
+  def explorer
+  end
+
+  def trade
+  end
+
+  def downloads
+  end
+
+  def news
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do  
-  # root to: 'home#show'
+  root to: 'pages#stats'
+
+  get '/pos' => 'pages#pos'
+  get '/wallets' => 'pages#wallets'
+  get '/explorer' => 'pages#explorer'
+  get '/trade' => 'pages#trade'
+  get '/downloads' => 'pages#downloads'
+  get '/news' => 'pages#news'
+
   scope path: 'admin' do
     authenticate :user, lambda { |u| u.admin? } do
       mount RailsEmailPreview::Engine, at: 'emails'
     end
   end
   
-
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
-  mount Thredded::Engine => '/'
+  mount Thredded::Engine => '/forum'
 end
